46da13e2c069bdc10b7fddb4621fcd8061add69b
MiryuuFurukawa <intecs.2607dp.06@gmail.com>
2026-08-27 07:55:21 +0900

    modify dropdown menu

--- javascript/header.js
+++ javascript/header.js
@@ -16,7 +16,21 @@ class RamenHeader extends HTMLElement {
                             <a href="menu.html">店舗情報▼</a>
                             <ul class="dropdown">
                                 <li><a href="store_hanabi.html">麺屋はなび</a></li>
-                                <li><a href="menu.html">店舗2</a></li>
+                                <li><a href="#">豚山</a></li>
+                                <li><a href="#">一番軒</a></li>
+                                <li><a href="#">歌志軒</a></li>
+                                <li><a href="#">きりめん</a></li>
+                                <li><a href="#">しげた</a></li>
+                                <li><a href="#">一蘭</a></li>
+                                <li><a href="#">ラーメン福</a></li>
+                                <li><a href="#">スガキヤ</a></li>
+                                <li><a href="#">来来亭</a></li>
+                                <li><a href="#">天下一品</a></li>
+                                <li><a href="#">町田商店</a></li>
+                                <li><a href="#">一風堂</a></li>
+                                <li><a href="#">蒙古タンメン中本</a></li>
+                                <li><a href="#">ラーメン二郎</a></li>
+                                <li><a href="#">魁力屋</a></li>
                             </ul>
                         </li>
                         <li><a href="rank.html">ランキング</a></li>
