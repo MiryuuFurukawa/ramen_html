7a9c047acfab375e7c9f450da5dfc093b209c379
Miryuu_Furukawa <intecs.2607dp.06@gmail.com>
2026-08-28 11:25:21 +0900

    add

--- javascript/loading.js
+++ javascript/loading.js
@@ -1,7 +1,6 @@
 window.addEventListener('load', () => {
     const loadingScreen = document.getElementById('loading');
     if (loadingScreen) {
-        // テスト用に3秒（3000ミリ秒）遅らせてから非表示にする
         setTimeout(() => {
             loadingScreen.classList.add('loaded');
         }, 1000);
